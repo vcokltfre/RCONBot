a5b109ac4a589c1dd5886ae411e1c08e5cd0cfef
vcokltfre <vcokltfre@gmail.com>
2020-08-03 11:35:00 +0100

    feat: add main bot file

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+from config.config import TOKEN
+
+
+class Bot(commands.Bot):
+    """A subclassed version of commands.Bot to add functionality"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def add_cog(self, cog: commands.Cog) -> None:
+        """Adds a cog to the bot and logs it."""
+        super().add_cog(cog)
+
+    def load_extensions(self, cogs: list):
+        """Loads a list of cogs"""
+        for cog in cogs:
+            try:
+                super().load_extension(cog)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    bot = Bot(
+        command_prefix="!",
+        help_command=commands.MinimalHelpCommand()
+    )
+
+    cogs = []
+
+    bot.load_extensions(cogs)
+    bot.run(TOKEN)
